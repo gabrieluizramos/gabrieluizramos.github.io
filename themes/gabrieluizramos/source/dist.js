0d926ad94b3ff339eaa96dad30c708e9186866cd
Gabriel Ramos <gabriel.luiz.ramos@gmail.com>
2017-10-27 11:51:14 -0200

    Bundle update

--- themes/gabrieluizramos/source/dist.js
+++ themes/gabrieluizramos/source/dist.js
@@ -65,13 +65,12 @@
 /************************************************************************/
 /******/ ([
 /* 0 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
+/***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__src_style_style_scss__ = __webpack_require__(1);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__src_style_style_scss___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0__src_style_style_scss__);
 
+
+__webpack_require__(1);
 
 /***/ }),
 /* 1 */
