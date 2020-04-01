911c9a9f314b81e1d7e7992c3d449cded95f119b
Gabriel Ramos <gabriel.luiz.ramos@gmail.com>
2020-04-01 15:02:51 -0300

    chore(commands): update preinstall command

--- commands/preinstall.sh
+++ commands/preinstall.sh
@@ -1,1 +1,8 @@
-npm i -g gatsby-cli
+echo "Running preinstall script"
+
+if [ -z $(which gatsby) ]; then
+    echo "No Gatsby CLI found"
+    npm i -g gatsby-cli
+else
+    echo "Gatsby CLI already installed"
+fi
