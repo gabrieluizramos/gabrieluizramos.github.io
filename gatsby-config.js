c256b788a59f5a4d2a48618f382f613842f268be
gabrieluizramos <gabriel.luiz.ramos@gmail.com>
2020-04-30 17:50:18 -0300

    refactor(sitemap): update sitemap to exclude draft posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,4 +1,4 @@
-const { filterDraftNodes } = require('./src/utils/drafts');
+const { filterDraftNodes, filterDraftPaths } = require('./src/utils/drafts');
 
 module.exports = {
   pathPrefix: '/blog',
@@ -93,7 +93,36 @@ module.exports = {
       },
     },
     ...require('@gabrieluizramos/preferences/gatsby/config'),
-    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-sitemap`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                siteUrl
+              }
+            }
+            allSitePage {
+              nodes {
+                path
+              }
+            }
+          }
+        `,
+        resolveSiteUrl: ({ site }) => site.siteMetadata.siteUrl,
+        serialize: ({ site, allSitePage }) => {
+          return allSitePage
+            .nodes
+            .filter(filterDraftPaths)
+            .map(node => ({
+              url: `${site.siteMetadata.siteUrl}${node.path}`,
+              changefreq: `daily`,
+              priority: 0.7,
+            }));
+        }
+      }
+    },
     {
       resolve: `gatsby-plugin-feed`,
       options: {
--- src/utils/drafts.js
+++ src/utils/drafts.js
@@ -3,3 +3,5 @@ const { parse } = require('query-string');
 exports.showDrafts = ({ location: { search } }) => parse(search).draft === 'true';
 
 exports.filterDraftNodes = ({ node: { fields: { sourceName } } }) => sourceName !== 'drafts';
+
+exports.filterDraftPaths = ({ path }) => !path.includes('draft');
