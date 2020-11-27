66d73799b8ca7e59833ffeb948b56e701a5f49d0
gabrieluizramos <gabriel.luiz.ramos@gmail.com>
2020-11-27 15:10:11 -0300

    chore(bio): update bio

--- src/components/bio/index.js
+++ src/components/bio/index.js
@@ -21,16 +21,13 @@ const Bio = ({ avatar }) => {
           Meu primeiro contato com tecnologia foi em meados de 2011 e, desde então, cada dia que passa tenho o prazer de aprender (e tento compartilhar) alguma coisa nova.
         </p>
         <p>
-          No aspecto profissional, iniciei minha carreira em 2014 e já tive a oportunidade de trabalhar com empresas de diversos tamanhos e segmentos: de e-commerces e companhias mais consolidadas (como Walmart e Mercado Livre) à startups unicórnios com produtos emergentes (como Loggi e, mais recentemente, Loft).
+          No aspecto profissional, iniciei minha carreira em 2014 e já tive a oportunidade de trabalhar com empresas de diversos tamanhos e segmentos: de e-commerces e companhias mais consolidadas à startups unicórnios com produtos emergentes.
         </p>
         <p>
           Na área acadêmica, cursei o ensino técnico em informática junto ao ensino médio no IFSP (Instituto Federal de São Paulo), me graduei como tecnólogo em Análise e Desenvolvimento de Sistemas pela FIAP (Faculdade de Informática e Administração Paulista) e atualmente curso MBA em Gestão de Projetos oferecido pela USP (Universidade de São Paulo). Nesse meio tempo tive a oportunidade de conhecer algumas outras instituições também.
         </p>
         <p>
-          Apesar disso, trabalhei por anos sem formação superior e, embora acredite que bases acadêmicas conseguem suprir algumas deficiências de conhecimentos computacionais (principalmente teóricos), defendo fortemente que as diversas formas de aprendizagem devem ser encorajadas. Acredito que não é uma faculdade que define a qualidade do trabalho de uma pessoa desenvolvedora.
-        </p>
-        <p>
-          É com isso em mente que também faço parte de empresas relacionadas à formação e instrução na área da tecnologia, atuando como instrutor na Caelum e mentor na Laboratória. Também já tive a oportunidade de participar de alguns outros eventos e projetos no ramo.
+          Apesar disso, trabalhei por anos sem formação superior e, embora acredite que bases acadêmicas conseguem suprir algumas deficiências de conhecimentos computacionais (principalmente teóricos), defendo fortemente que as diversas formas de aprendizagem devem ser encorajadas. Acredito que não é uma faculdade que define a qualidade do trabalho de uma pessoa desenvolvedora. É por isso que também faço parte de projetos relacionadas à formação e instrução na área da tecnologia.
         </p>
         <p>
           Fora isso, gosto muito de fotografia e música e, nas horas vagas, quando não estou vendo algum conteúdo relacionado à esses hobbies, provavelmente estou jogando video-game ou assistindo alguma série e brincando com meus gatos.
